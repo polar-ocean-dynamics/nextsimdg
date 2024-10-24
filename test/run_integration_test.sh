9e85f01adb0c4f81eb9d2ad3782326773f909462
Tim Spain <timothy.spain@nersc.no>
2024-10-24 14:43:00 +0200

    Look for the binary in the correct location.

--- test/run_integration_test.sh
+++ test/run_integration_test.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-if [ ! -f nextsim ]; then
-    echo "Copy or link the nextsim executable here from the build directory"
+if [ ! -f ../nextsim ]; then
+    echo "Copy or link the nextsim executableinto the parent directory from the build directory"
 fi
-./nextsim --config-file integration_test.cfg "$@"
+../nextsim --config-file integration_test.cfg "$@"
