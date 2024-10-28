8bfde301a12bff189bc3c649f235d8fca48d4ef8
Tim Spain <timothy.spain@nersc.no>
2024-10-28 15:36:24 +0100

    Pass a python command. Correct sh syntax for a script.

--- test/run_test_jan2010_integration_test.sh
+++ test/run_test_jan2010_integration_test.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# Get the name of the python executable
+if [ $# -lt 1 ]
+then
+    PYTHON=python
+else
+    PYTHON=$1
+fi
+
+
 # Prepares, executes and test the nextSIM-DG integration test based on the
 # January 2010 Arctic simulation
 
@@ -8,13 +17,13 @@ era5_file=25km_NH.ERA5_2010-01-01T000000_test_data.nc
 topaz_file=25km_NH.TOPAZ4_2010-01-01T000000_test_data.nc
 out_file=out.integration_test.nc
 
-python make_init25kmNH_test_data.py
-python era5_topaz4_test_data.py
+$PYTHON make_init25kmNH_test_data.py
+$PYTHON era5_topaz4_test_data.py
 echo run_integration_test.sh
 ./run_integration_test.sh
-python test_integration_test.py
+$PYTHON test_integration_test.py
 test_return_value=$?
 rm $restart_file $era5_file $topaz_file $out_file
 rm nextsim.*.log
 
-return $test_return_value
+exit $test_return_value
