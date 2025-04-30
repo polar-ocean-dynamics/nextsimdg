03a8dc047cf65bd54393209eb16630b3b260dcee
Einar Örn Ólason <einar.olason@nersc.no>
2025-04-30 11:35:52 +0200

    Fix open-water flux in FluxConfiguredAtmosphere
    The open water heat flux is reset by the thermodynamics, that the
    (slab) ocean doesn't super cool. This commit sets it back to the initial
    value on every FluxConfiguredAtmosphere::update call.

--- physics/src/modules/AtmosphereBoundaryModule/include/FluxConfiguredAtmosphere.hpp
+++ physics/src/modules/AtmosphereBoundaryModule/include/FluxConfiguredAtmosphere.hpp
@@ -1,7 +1,7 @@
 /*!
  * @file FluxConfiguredAtmosphere.hpp
  *
- * @date Sep 29, 2022
+ * @date 30 Apr 2025
  * @author Tim Spain <timothy.spain@nersc.no>
  */
 
@@ -42,8 +42,8 @@ public:
     ConfigMap getConfiguration() const override;
 
 protected:
-    //! Performs the implementation specific updates. Does nothing.
-    void update(const TimestepTime&) override { }
+    //! Performs the implementation specific updates.
+    void update(const TimestepTime&) override;
 
 private:
     static double qia0;
