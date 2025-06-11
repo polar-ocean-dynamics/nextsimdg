b84f8a1b20b98a94a9d987aa48bf047b883def8c
Einar Örn Ólason <einar.olason@nersc.no>
2025-06-11 17:04:59 +0200

    Revert "I made a mistake editing the comment"
    This reverts commit b0ad5cf86e6082d2134c43a9b8ec2b286009cb63.

--- physics/src/modules/include/ILateralIceSpread.hpp
+++ physics/src/modules/include/ILateralIceSpread.hpp
@@ -26,11 +26,10 @@ public:
         newice.resize();
         snowMelt.resize();
 
-        /*
-         * Set these to zero, so we don't have uninitialized values floating around.
-         * As long as we have the nextsim_thermo.use_thermo_forcing option, all
-         * derived classes must initialise these values to something sensible.
-         */
+        /* Set these to zero, so we don't have uninitialized values floating around.
+         * This could be done in the derived classes, but it is convenient to do it here because
+         * since we have the nextsim_thermo.use_thermo_forcing option, all derived classes must
+         * initialise these values to something sensible.*/
         deltaCIce = 0.;
         newice = 0.;
         snowMelt = 0.;
